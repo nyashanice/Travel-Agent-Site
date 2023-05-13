5f5cbd8794966f2667337dd6763ace3818d3e537
Nya Tyus <nyastyus@gmail.com>
2023-05-13 18:53:08 -0400

    create media queries for buffer

--- src/components/Buffer.js
+++ src/components/Buffer.js
@@ -4,35 +4,37 @@ import "../styles/Buffer.css";
 
 export default function Buffer() {
   return (
-    <div className="d-flex flex-row">
-      <div>
-        <img
-          src="../Travel-Agent-Site/images/desk.png"
-          alt=""
-          className="buffer-img m-5"
-        />
-      </div>
-      <div className="p-5">
-        <ul className="buffer-txt">
-          <li className="p-3">Wanting to explore but not sure what to do?</li>
-          <li className="p-3">Need a getaway?</li>
-          <li className="p-3">Just not sure where to start?</li>
-        </ul>
-        <h4 className="p-2 buffer-txt">
-          Voyage Vistas has you covered! Our experienced travel agents are ready
-          to take on any questions you may have and ensure that your vacation
-          planning is seamless.
-        </h4>
-        <div className="ml-0 mt-5">
-          <Button
-            variant="secondary"
-            size="lg"
-            className="cta-btn"
-            href="#services"
-          >
-            See What We Offer
-          </Button>
+    <div>
+      <div className="d-flex flex-row flex-wrap align-items-center">
+        <div>
+          <img
+            src="../Travel-Agent-Site/images/desk.png"
+            alt=""
+            className="buffer-img p-3"
+          />
         </div>
+        <div className="p-2">
+          <ul className="buffer-txt">
+            <li className="p-1">Wanting to explore but not sure what to do?</li>
+            <li className="p-1">Need a getaway?</li>
+            <li className="p-1">Just not sure where to start?</li>
+          </ul>
+        </div>
+      </div>
+      <h4 className="p-3 pt-0 buffer-txt">
+        Voyage Vistas has you covered! Our experienced travel agents are ready
+        to take on any questions you may have and ensure that your vacation
+        planning is seamless.
+      </h4>
+      <div className="p-3">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="cta-btn"
+          href="#services"
+        >
+          See What We Offer
+        </Button>
       </div>
     </div>
   );
